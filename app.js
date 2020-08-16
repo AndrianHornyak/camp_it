0e67f631a25610bd9139e37ea069233e10257f4e
AndrianHornyak <muyrux@adrianhornyaks-MacBook-Pro.local>
2020-08-16 09:49:10 +0300

    fix commit

--- app.js
+++ app.js
@@ -1,60 +1,67 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const router = require('./middleware/router.js')
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const router = require('./middleware/router.js');
 //Routers
 
-mongoose.connect('mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@node-rest-api-b9rbq.mongodb.net/test?retryWrites=true&w=majority', {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-    },
-    () => {
-        console.log('\n connected database... \n')
-    }
-)
-mongoose.Promise = global.Promise
+mongoose.connect(
+  'mongodb+srv://node-shop:' +
+    process.env.MONGO_ATLAS_PW +
+    '@node-rest-api-b9rbq.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  () => {
+    console.log('\n connected database... \n');
+  }
+);
+mongoose.Promise = global.Promise;
 
-app.use(morgan('dev'))
-app.use('/uploads', express.static('uploads'))
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(bodyParser.json())
+app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-type, Accept, Authorization')
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, GET, DELETE, PATCH, POST')
-        return res.status(200).json({})
-    }
-    next()
-})
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, DELETE, PATCH, POST');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 //Routes which should handle requests
-app.use(router)
+app.use(router);
 
 app.get('/', (req, res) => {
-    res.send('We are on home page')
-})
+  res.send('We are on home page');
+});
 
 app.use((req, res, next) => {
-    const error = new Error('Not found')
-    error.status = 404
-    next(error)
-})
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
-module.exports = app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const http = require('http')
-const app = require('./app.js')
+const http = require('http');
+const app = require('./app.js');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(PORT, async () => {
-    console.log(
-        `\n Server listening on port: ${PORT}...`
-    )
-})
+  console.log(`\n Server listening on port: ${PORT}...`);
+});
